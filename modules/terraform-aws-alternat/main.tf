d166489496ffcad05327ffc1125ad38d2ea60d29
Amey Bhide <ameyb@splunk.com>
2023-03-14 14:22:15 -0700

    Fixes

--- modules/terraform-aws-alternat/main.tf
+++ modules/terraform-aws-alternat/main.tf
@@ -111,8 +111,7 @@ resource "aws_autoscaling_group" "nat_instance" {
   }
 
   provisioner "local-exec" {
-    for_each = { for obj in var.vpc_az_maps : obj.az => obj.nat_gateway_id }
-    command = var.create_nat_gateways ? "" : "echo ${each.value}"
+    command = var.create_nat_gateways ? "" : "echo ${each.nat_gateway_id}"
   }
 }
 
